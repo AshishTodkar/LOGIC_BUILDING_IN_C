606df6e6ec7da29f4faf7d33770216bc31a10b2a
AshishTodkar <ashishmali8402@gmail.com>
2022-11-02 15:26:08 +0530

    First Assignment

---
+++ ASSIGNMENT NO. 01/02 Program To Print 5 Times Marvellous On Screen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Display()
+{
+    int i = 0;
+
+    for(i=1;i<=5;i++)
+    {
+        printf("\n Marvellous");
+    }
+}
+
+int main()
+{
+    Display();
+
+    getch();
+    return 0;
+}
