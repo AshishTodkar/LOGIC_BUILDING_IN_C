38a7f92617398ee0e6c7d6407d7886bb4d558519
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-11-06 19:59:31 +0530

    Fifth Assignment

---
+++ ASSIGNMENT NO. 05/03 Accept Number From User And Display Its Non Factors.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void NonFact(int iNo)
+{
+    int i = 0;
+
+    for(i=1;i<iNo;i++)
+    {
+        if(iNo % i != 0)
+        {
+            printf("\t %d",i);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("\n Enter A Number : ");
+    scanf("%d",&iValue);
+
+    NonFact(iValue);
+
+    getch();
+    return 0;
+}
