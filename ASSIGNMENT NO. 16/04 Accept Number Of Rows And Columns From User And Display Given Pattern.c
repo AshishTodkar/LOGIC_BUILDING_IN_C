ea2446d7bb49de439e9df1ee2a46409747b75470
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-11-17 19:33:53 +0530

    Sixteenth Assignment

---
+++ ASSIGNMENT NO. 16/04 Accept Number Of Rows And Columns From User And Display Given Pattern.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int iNo1,int iNo2)
+{
+    int r = 0 , c = 0;
+
+    for(r=iNo1;r>=1;r--)
+    {
+        for(c=1;c<=iNo2;c++)
+        {
+            printf("\t %d",r);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("\n Enter A Number Of Rows : ");
+    scanf("%d",&iValue1);
+
+    printf("\n Enter A Number Of Columns : ");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    getch();
+    return 0;
+}
