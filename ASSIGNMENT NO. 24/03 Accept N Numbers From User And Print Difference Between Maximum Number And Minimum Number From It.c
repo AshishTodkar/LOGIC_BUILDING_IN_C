290421f3820853d00b76c9231885b09572befbf3
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-11-25 18:31:52 +0530

    Twenty-Fourth Assignment

---
+++ ASSIGNMENT NO. 24/03 Accept N Numbers From User And Print Difference Between Maximum Number And Minimum Number From It.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Difference(int Arr[], int iLength)
+{
+    int i = 0, Min = 0, Max = 0;
+
+    Min = Arr[i];
+
+    for(i=0;i<iLength;i++)
+    {
+        if( Arr[i] < Min )
+        {
+            Min = Arr[i];
+        }
+    }
+
+    for(i=0;i<iLength;i++)
+    {
+        if( Arr[i] > Max )
+        {
+            Max = Arr[i];
+        }
+    }
+
+    return Max - Min;
+}
+
+int main()
+{
+    int  *p = NULL, iSize = 0, iRet = 0, iCnt = 0;
+
+    printf("\n\t Enter Number Of Elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+         printf("\n\t Unable to allocate Memory. ");
+         return -1;
+    }
+
+    printf("\n\t Enter %d Elements : \n",iSize);
+
+    for(iCnt = 0;iCnt<iSize;iCnt++)
+    {
+         printf("\n\t Enter element %d : ",iCnt+1);
+         scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Difference(p, iSize);
+
+    printf("\n\t Difference Between Maximum Number And Minimum Number Is %d.",iRet);
+
+    free(p);
+
+    getch();
+    return 0;
+}
