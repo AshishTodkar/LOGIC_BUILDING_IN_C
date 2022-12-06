70748a970740620dc02b67d352ee99c66f1de77c
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-12-06 22:41:12 +0530

    Thirtieth Assignment

---
+++ ASSIGNMENT NO. 30/01 Accept String From User And Copy The Content Of That String Into Another String .c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void StrCpy(char *Str1, char *Str2)
+{
+    while(*Str1 != '\0')
+    {
+        *Str2 = *Str1;
+        Str1++;
+        Str2++;
+    }
+
+    *Str2 = '\0';
+
+    return;
+
+}
+
+int main()
+{
+    char Arr1[20], Arr2[20];
+
+    printf("\n\t Enter A String => ");
+    scanf("%[^\n]",&Arr1);
+
+    StrCpy(Arr1,Arr2);
+
+    printf("\n\t After Copying String Is : %s",Arr2);
+
+    getch();
+    return 0;
+}
